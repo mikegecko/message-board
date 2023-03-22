04ffeaa78b651775616aa2c9037d3d93d41d34ce
Michael Guedko <mjgeko@gmail.com>
2023-03-22 16:17:58 -0400

    Add temp messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text:"Hi there!",
+    user: "Bobathy",
+    added: new Date()
+  },
+  {
+    text: "Hello world!",
+    user: "Max",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 module.exports = router;
