7688cd0cbec939dfda8653806d8287dc5620052d
Michael Guedko <mjgeko@gmail.com>
2023-03-28 13:09:25 -0400

    Remove log statements

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it is working
+//console.log(process.env) // remove this after you've confirmed it is working
 
 var createError = require('http-errors');
 var express = require('express');
